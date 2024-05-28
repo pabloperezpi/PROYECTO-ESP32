223913d8b8fe58dcfbbffaf99b9828eb8e11cae7
pabloperezpi <160481135+pabloperezpi@users.noreply.github.com>
2024-05-28 10:25:11 +0200

    Update main.cpp

--- Proyecto_PD/src/main.cpp
+++ Proyecto_PD/src/main.cpp
@@ -2,18 +2,28 @@
 #include <WebServer.h>
 #include <Adafruit_NeoPixel.h>
 
+// Definición de constantes
+#define NUM_LEDS 50 // Número de LEDs en la tira LED
+#define MIC_PIN A0  // Pin del micrófono
+#define LED_PIN 27  // Pin de la tira LED
 #define NUM_LEDS 144
 #define LED_PIN 14
 #define MICROPHONE_PIN 34
 #define NUM_SAMPLES 10
 
+// Creación de un objeto para controlar la tira LED NeoPixel
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+// Credenciales WiFi
+const char* ssid = "iPhone de Pablo";
+const char* password = "pabloperez";
 const char* ssid = "iPhone de AyalaKT";
 const char* password = "12345678";
 
+// Configuración del servidor web
 WebServer server(80);
 
+// Declaración de las funciones que manejarán las solicitudes HTTP
 void handleToggle();
 void handleColor();
 void handleBrightness();
@@ -26,12 +36,14 @@ void showRainbow();
 void showBlink(uint32_t color, int brightness, int delayTime);
 void showRandomRainbowBlink(int brightness, int delayTime);
 
+// Enumeración para los modos de la tira LED
 enum LedMode { OFF, STATIC_COLOR, RAINBOW, BLINK, RAINBOW_BLINK };
 LedMode currentMode = OFF;
 
+// Variables para el estado de las luces LED
 bool isOn = false;
+String selectedColor = "#ff0000"; // Color por defecto
 bool microphoneActive = false;
-String selectedColor = "#ff0000";
 int brightness = 11;
 
 // Variables para el procesamiento del micrófono
@@ -44,25 +56,26 @@ int total = 0;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(MIC_PIN, INPUT);
   strip.begin();
+  strip.show(); // Inicializa la tira LED apagada
   strip.setBrightness(brightness);
   strip.show();
 
+  // Conexión a la red WiFi
   Serial.println("Intentando conectar a ");
   Serial.println(ssid);
-
   WiFi.begin(ssid, password);
 
+  // Espera hasta que se conecte a WiFi
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(".");
+    Serial.println("Conectando...");
   }
 
-  Serial.println("");
-  Serial.println("WiFi Conectado");
-  Serial.print("IP obtenida: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("Conectado a WiFi");
 
+  // Configuración de las rutas del servidor web
   server.on("/", HTTP_GET, handleRoot);
   server.on("/toggle", HTTP_GET, handleToggle);
   server.on("/color", HTTP_GET, handleColor);
@@ -71,8 +84,8 @@ void setup() {
   server.on("/microphone", HTTP_GET, handleMicrophone);
   server.on("/micValue", HTTP_GET, handleMicValue);
 
+  // Inicia el servidor web
   server.begin();
-
   Serial.println("Servidor HTTP iniciado");
 
   // Calibración inicial del micrófono
@@ -86,12 +99,14 @@ void setup() {
   Serial.print("Nivel de ruido ambiental calibrado: ");
   Serial.println(noiseBase);
 
+  // Inicialización del array de valores del micrófono
   for (int i = 0; i < smoothingSamples; i++) {
     micValues[i] = noiseBase;
     total += micValues[i];
   }
 }
 
+// Función para obtener el valor suavizado del micrófono
 int getSmoothedMicValue() {
   int micValue = analogRead(MICROPHONE_PIN);
 
@@ -114,7 +129,7 @@ int getSmoothedMicValue() {
 }
 
 void loop() {
-  server.handleClient();
+  server.handleClient(); // Maneja las solicitudes HTTP entrantes
 
   if (isOn) {
     if (microphoneActive) {
@@ -126,6 +141,10 @@ void loop() {
     }
 
     uint32_t color = parseColor(selectedColor);
+    for (int i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, color);
+    }
+    strip.show();
 
     switch (currentMode) {
       case STATIC_COLOR:
@@ -154,15 +173,17 @@ void loop() {
     strip.show();
   }
 
-  delay(100);
+  delay(100); // Intervalo de actualización
 }
 
+// Función para parsear un color en formato hexadecimal a RGB
 uint32_t parseColor(String color) {
-  color.remove(0, 1);
+  color.remove(0, 1); // Quitar el '#'
   long number = strtol(color.c_str(), NULL, 16);
   return strip.Color((number >> 16) & 0xFF, (number >> 8) & 0xFF, number & 0xFF);
 }
 
+// Función para mostrar un efecto arcoíris
 void showRainbow() {
   static uint16_t j = 0;
   for (uint16_t i = 0; i < strip.numPixels(); i++) {
@@ -172,6 +193,7 @@ void showRainbow() {
   j++;
 }
 
+// Función para mostrar un efecto de parpadeo
 void showBlink(uint32_t color, int brightness, int delayTime) {
   strip.setBrightness(brightness);
   strip.fill(color);
@@ -182,6 +204,7 @@ void showBlink(uint32_t color, int brightness, int delayTime) {
   delay(delayTime);
 }
 
+// Función para mostrar un efecto de parpadeo arcoíris
 void showRandomRainbowBlink(int brightness, int delayTime) {
   strip.setBrightness(brightness);
   for (int i = 0; i < strip.numPixels(); i++) {
@@ -195,284 +218,180 @@ void showRandomRainbowBlink(int brightness, int delayTime) {
   delay(delayTime);
 }
 
+// Código HTML para la interfaz de usuario
 String HTML = R"=====(
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Control de luces LED</title>
+    <title>Control de Luces LED</title>
     <style>
         body {
             font-family: Arial, sans-serif;
             background-color: #f0f0f0;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
             text-align: center;
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 50px;
         }
-
         .btn {
-            display: inline-block;
             padding: 10px 20px;
             font-size: 16px;
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
+            margin: 10px;
             cursor: pointer;
-            transition: background-color 0.3s ease;
         }
-
-        .btn:hover {
-            background-color: #0056b3;
-        }
-
-        .color-palette {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            gap: 5px;
-            margin-top: 20px;
-        }
-
         .color-box {
-            width: 50px;
-            height: 50px;
+            width: 30px;
+            height: 30px;
+            display: inline-block;
+            margin: 5px;
             border-radius: 50%;
             cursor: pointer;
         }
-
-        .color-box.rainbow {
-            background: linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet);
-        }
-
         .brightness-slider {
             margin-top: 20px;
         }
-
         .slider {
             width: 100%;
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <h1>Control de luces LED</h1>
-        <div>
-            <h2>Encender / Apagar</h2>
-            <button id="toggleButton" class="btn">Encender</button>
-        </div>
-        <div>
-            <h2>Modo</h2>
-            <button id="fixedModeButton" class="btn">Color fijo</button>
-            <button id="blinkModeButton" class="btn">Parpadeo</button>
-            <button id="rainbowModeButton" class="btn">Arcoíris</button>
-            <button id="rainbowBlinkModeButton" class="btn">Parpadeo Arcoíris</button>
-        </div>
-        <div>
-            <h2>Colores</h2>
-            <div class="color-palette">
-                <div class="color-box" style="background-color: #FF5733;" onclick="setColor('#FF5733')"></div>
-                <div class="color-box" style="background-color: #FFC300;" onclick="setColor('#FFC300')"></div>
-                <div class="color-box" style="background-color: #C70039;" onclick="setColor('#C70039')"></div>
-                <div class="color-box" style="background-color: #900C3F;" onclick="setColor('#900C3F')"></div>
-                <div class="color-box" style="background-color: #581845;" onclick="setColor('#581845')"></div>
-                <!-- Aquí van más cajas de colores -->
-            </div>
-        </div>
-        <div class="brightness-slider">
-            <h2>Brillo</h2>
-            <input type="range" min="1" max="255" value="11" class="slider" id="brightnessSlider">
-        </div>
-        <div>
-            <h2>Micrófono</h2>
-            <button id="microphoneButton" class="btn">Activar Micrófono</button>
-            <p id="micValue">Valor del Micrófono: </p>
+    <div>
+        <h1>Control de Luces LED</h1>
+        <button id="toggleButton" class="btn">Encender</button>
+    </div>
+    <div>
+        <h2>Modo</h2>
+        <button id="fixedModeButton" class="btn">Color fijo</button>
+        <button id="blinkModeButton" class="btn">Parpadeo</button>
+        <button id="rainbowModeButton" class="btn">Arcoíris</button>
+        <button id="rainbowBlinkModeButton" class="btn">Parpadeo Arcoíris</button>
+    </div>
+    <div>
+        <h2>Colores</h2>
+        <div class="color-palette">
+            <div class="color-box" style="background-color: #ff0000;"></div>
+            <div class="color-box" style="background-color: #ff8000;"></div>
+            <div class="color-box" style="background-color: #ffff00;"></div>
+            <div class="color-box" style="background-color: #00ff00;"></div>
+            <div class="color-box" style="background-color: #0000ff;"></div>
+            <div class="color-box" style="background-color: #4b0082;"></div>
+            <div class="color-box" style="background-color: #9400d3;"></div>
+            <div class="color-box" style="background-color: #ffffff;"></div>
         </div>
     </div>
+    <div class="brightness-slider">
+        <h2>Brillo</h2>
+        <input type="range" id="brightnessSlider" class="slider" min="1" max="255" value="11">
+    </div>
+    <div>
+        <h2>Micrófono</h2>
+        <button id="microphoneButton" class="btn">Activar</button>
+    </div>
     <script>
-    const toggleButton = document.getElementById('toggleButton');
-    const fixedModeButton = document.getElementById('fixedModeButton');
-    const blinkModeButton = document.getElementById('blinkModeButton');
-    const rainbowModeButton = document.getElementById('rainbowModeButton');
-    const rainbowBlinkModeButton = document.getElementById('rainbowBlinkModeButton');
-    const brightnessSlider = document.getElementById('brightnessSlider');
-    const microphoneButton = document.getElementById('microphoneButton');
-    const micValueDisplay = document.getElementById('micValue');
+        document.getElementById('toggleButton').addEventListener('click', function() {
+            fetch('/toggle')
+                .then(response => response.text())
+                .then(state => {
+                    document.getElementById('toggleButton').innerText = state === 'on' ? 'Apagar' : 'Encender';
+                });
+        });
 
-    toggleButton.addEventListener('click', () => {
-        const isOn = toggleButton.innerText === 'Encender';
-        fetch(`/toggle?state=${isOn ? 'on' : 'off'}`)
-            .then(response => response.json())
-            .then(data => {
-                toggleButton.innerText = isOn ? 'Apagar' : 'Encender';
-            })
-            .catch(error => console.error('Error:', error));
-    });
+        document.querySelectorAll('.color-box').forEach(function(element) {
+            element.addEventListener('click', function() {
+                let color = this.style.backgroundColor;
+                color = rgbToHex(color);
+                fetch('/color?value=' + color)
+                    .then(response => response.text());
+            });
+        });
 
-    fixedModeButton.addEventListener('click', () => {
-        fetch('/mode?value=fixed')
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
-    });
+        document.getElementById('brightnessSlider').addEventListener('input', function() {
+            let brightness = this.value;
+            fetch('/brightness?value=' + brightness)
+                .then(response => response.text());
+        });
 
-    blinkModeButton.addEventListener('click', () => {
-        fetch('/mode?value=blink')
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
-    });
+        document.getElementById('fixedModeButton').addEventListener('click', function() {
+            fetch('/mode?value=fixed')
+                .then(response => response.text());
+        });
 
-    rainbowModeButton.addEventListener('click', () => {
-        fetch('/mode?value=rainbow')
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
-    });
+        document.getElementById('blinkModeButton').addEventListener('click', function() {
+            fetch('/mode?value=blink')
+                .then(response => response.text());
+        });
 
-    rainbowBlinkModeButton.addEventListener('click', () => {
-        fetch('/mode?value=rainbow_blink')
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
-    });
+        document.getElementById('rainbowModeButton').addEventListener('click', function() {
+            fetch('/mode?value=rainbow')
+                .then(response => response.text());
+        });
 
-    brightnessSlider.addEventListener('input', () => {
-        const brightness = brightnessSlider.value;
-        fetch(`/brightness?value=${brightness}`)
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
-    });
+        document.getElementById('rainbowBlinkModeButton').addEventListener('click', function() {
+            fetch('/mode?value=rainbowblink')
+                .then(response => response.text());
+        });
 
-    microphoneButton.addEventListener('click', () => {
-        const isActive = microphoneButton.innerText === 'Activar Micrófono';
-        fetch(`/microphone?state=${isActive ? 'on' : 'off'}`)
-            .then(response => response.json())
-            .then(data => {
-                microphoneButton.innerText = isActive ? 'Desactivar Micrófono' : 'Activar Micrófono';
-                updateMicValue();
-            })
-            .catch(error => console.error('Error:', error));
-    });
+        document.getElementById('microphoneButton').addEventListener('click', function() {
+            fetch('/microphone')
+                .then(response => response.text())
+                .then(state => {
+                    document.getElementById('microphoneButton').innerText = state === 'on' ? 'Desactivar' : 'Activar';
+                });
+        });
 
-    function setColor(color) {
-        fetch(`/color?value=${encodeURIComponent(color)}`)
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
-    }
-
-    function updateMicValue() {
-        if (microphoneButton.innerText === 'Desactivar Micrófono') {
-            setInterval(() => {
-                fetch('/micValue')
-                    .then(response => response.json())
-                    .then(data => {
-                        micValueDisplay.innerText = `Valor del Micrófono: ${data.value}`;
-                    })
-                    .catch(error => console.error('Error:', error));
-            }, 1000);
+        function rgbToHex(rgb) {
+            let result = rgb.match(/\d+/g);
+            let r = parseInt(result[0]).toString(16).padStart(2, '0');
+            let g = parseInt(result[1]).toString(16).padStart(2, '0');
+            let b = parseInt(result[2]).toString(16).padStart(2, '0');
+            return '#' + r + g + b;
         }
-    }
     </script>
 </body>
 </html>
 )=====";
 
+// Manejo de solicitudes HTTP
 void handleRoot() {
   server.send(200, "text/html", HTML);
 }
 
 void handleToggle() {
-  if (server.hasArg("state")) {
-    String state = server.arg("state");
-    Serial.print("Toggle state: ");
-    Serial.println(state);
-    if (state == "on") {
-      isOn = true;
-    } else if (state == "off") {
-      isOn = false;
-    }
-    server.send(200, "application/json", "{\"message\":\"Toggle command received\"}");
-  } else {
-    Serial.println("Error: 'state' parameter missing");
-    server.send(400, "application/json", "{\"error\":\"Missing 'state' parameter\"}");
-  }
+  isOn = !isOn;
+  server.send(200, "text/plain", isOn ? "on" : "off");
 }
 
 void handleColor() {
-  if (server.hasArg("value")) {
-    selectedColor = server.arg("value");
-    Serial.print("Color seleccionado: ");
-    Serial.println(selectedColor);
-    server.send(200, "application/json", "{\"message\":\"Color command received\"}");
-  } else {
-    Serial.println("Error: 'value' parameter missing");
-    server.send(400, "application/json", "{\"error\":\"Missing 'value' parameter\"}");
-  }
+  selectedColor = server.arg("value");
+  server.send(200, "text/plain", selectedColor);
 }
 
 void handleBrightness() {
-  if (server.hasArg("value")) {
-    brightness = server.arg("value").toInt();
-    Serial.print("Brightness set to: ");
-    Serial.println(brightness);
-    server.send(200, "application/json", "{\"message\":\"Brightness command received\"}");
-  } else {
-    Serial.println("Error: 'value' parameter missing");
-    server.send(400, "application/json", "{\"error\":\"Missing 'value' parameter\"}");
-  }
+  brightness = server.arg("value").toInt();
+  server.send(200, "text/plain", String(brightness));
 }
 
 void handleMode() {
-  if (server.hasArg("value")) {
-    String modeValue = server.arg("value");
-    Serial.print("Mode value: ");
-    Serial.println(modeValue);
-    if (modeValue == "fixed") {
-      currentMode = STATIC_COLOR;
-    } else if (modeValue == "blink") {
-      currentMode = BLINK;
-    } else if (modeValue == "rainbow") {
-      currentMode = RAINBOW;
-    } else if (modeValue == "rainbow_blink") {
-      currentMode = RAINBOW_BLINK;
-    }
-    server.send(200, "application/json", "{\"message\":\"Mode command received\"}");
-  } else {
-    Serial.println("Error: 'value' parameter missing");
-    server.send(400, "application/json", "{\"error\":\"Missing 'value' parameter\"}");
+  String mode = server.arg("value");
+  if (mode == "fixed") {
+    currentMode = STATIC_COLOR;
+  } else if (mode == "rainbow") {
+    currentMode = RAINBOW;
+  } else if (mode == "blink") {
+    currentMode = BLINK;
+  } else if (mode == "rainbowblink") {
+    currentMode = RAINBOW_BLINK;
   }
+  server.send(200, "text/plain", mode);
 }
 
 void handleMicrophone() {
-  if (server.hasArg("state")) {
-    String state = server.arg("state");
-    Serial.print("Microphone state: ");
-    Serial.println(state);
-    if (state == "on") {
-      microphoneActive = true;
-    } else if (state == "off") {
-      microphoneActive = false;
-    }
-    server.send(200, "application/json", "{\"message\":\"Microphone command received\"}");
-  } else {
-    Serial.println("Error: 'state' parameter missing");
-    server.send(400, "application/json", "{\"error\":\"Missing 'state' parameter\"}");
-  }
+  microphoneActive = !microphoneActive;
+  server.send(200, "text/plain", microphoneActive ? "on" : "off");
 }
 
 void handleMicValue() {
-  int adjustedMicValue = getSmoothedMicValue();
-  String json = "{\"value\":" + String(adjustedMicValue) + "}";
-  server.send(200, "application/json", json);
+  int micValue = getSmoothedMicValue();
+  server.send(200, "text/plain", String(micValue));
 }
-
-
